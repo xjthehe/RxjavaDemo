7544e29b3477757fe46ebc6cfb83c2e0e9540567
xjthehe <617490820@qq.com>
2018-12-14 00:23:36 +0800

    Initial commit

---
+++ app/src/main/java/com/pax/rowen/rxjavademo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.pax.rowen.rxjavademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rx.Observable;
+import rx.Subscriber;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //被观察者
+        Observable<String> myObservable = Observable.create(
+                new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> sub) {
+                        sub.onNext("Hello, world!");
+                        sub.onCompleted();
+                    }
+                }
+        );
+
+        Subscriber<String> mySubscriber=new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+        };
+
+
+
+
+
+
+
+
+    }
+}
